0d222e31f36c584d36a255aba9e0ecc3f6588d6f
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-04 06:32:24 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 (crontab -l ; echo "0 0 * * * wget https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash") | crontab -
 wget https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash
+read -n 1 -s -r -p "برای پایان، یک دکمه را فشار  ..."
