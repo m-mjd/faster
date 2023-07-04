d509a3dda058446a11946d400e9e352b233b1447
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-04 06:31:44 -0700

    faster.sh

--- faster.sh
+++ faster.sh
@@ -52,5 +52,4 @@ sudo sed -i '$a net.ipv4.icmp_echo_ignore_all=1' /etc/sysctl.conf
 chmod +x /usr/local/bin/update_resolv_conf.sh
 sudo sysctl -p
 sudo systemctl restart systemd-resolved.service
-clear
 echo "Finish"
