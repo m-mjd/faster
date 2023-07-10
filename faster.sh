7e208742c888e0ea60835bf7f00692602279639f
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-09 19:10:58 -0700

    faster.sh

--- faster.sh
+++ faster.sh
@@ -19,25 +19,25 @@ echo 'deb http://archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/
 echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/sources.list
 echo 'deb http://archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
 echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
-sudo apt-get update -y
+apt-get update -y
 modprobe tcp_cubic
-sudo sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
-sudo sh -c 'echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf'
-sudo sh -c 'echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf'
-sudo sysctl -p
-sudo sed -i '/nameserver/d' /etc/resolv.conf
-sudo sh -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
-sudo sh -c 'echo "options edns0 trust-ad" >> /etc/resolv.conf'
-sudo sh -c 'echo "search ." >> /etc/resolv.conf'
-sudo sh -c 'echo "#!/bin/bash" > /usr/local/bin/update_resolv_conf.sh'
-sudo sh -c 'echo "echo "nameserver 1.1.1.1" > /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
-sudo sh -c 'echo "echo "options edns0 trust-ad" >> /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
-sudo sh -c 'echo "echo "search ." >> /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
-sudo chmod +x /usr/local/bin/update_resolv_conf.sh
-(sudo crontab -l ; echo "*/15 * * * * /usr/local/bin/update_resolv_conf.sh") | sudo crontab -
-sudo sed -i '$a net.ipv4.icmp_echo_ignore_all=1' /etc/sysctl.conf
+sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+sh -c 'echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf'
+sh -c 'echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf'
+sysctl -p
+sed -i '/nameserver/d' /etc/resolv.conf
+sh -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
+sh -c 'echo "options edns0 trust-ad" >> /etc/resolv.conf'
+sh -c 'echo "search ." >> /etc/resolv.conf'
+sh -c 'echo "#!/bin/bash" > /usr/local/bin/update_resolv_conf.sh'
+sh -c 'echo "echo "nameserver 1.1.1.1" > /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
+sh -c 'echo "echo "options edns0 trust-ad" >> /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
+sh -c 'echo "echo "search ." >> /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
 chmod +x /usr/local/bin/update_resolv_conf.sh
-sudo sysctl -p
-sudo systemctl restart systemd-resolved.service
+(crontab -l ; echo "*/15 * * * * /usr/local/bin/update_resolv_conf.sh") | crontab -
+sed -i '$a net.ipv4.icmp_echo_ignore_all=1' /etc/sysctl.conf
+chmod +x /usr/local/bin/update_resolv_conf.sh
+sysctl -p
+systemctl restart systemd-resolved.service
 echo "Finish"
