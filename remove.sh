04080182c5df04ff8095afe122ff1c6e6eae7b84
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-17 13:54:22 -0700

    remove.sh

--- remove.sh
+++ remove.sh
@@ -4,9 +4,6 @@ sed -i '/net.ipv4.icmp_echo_ignore_all=1/d' /etc/sysctl.conf
 sed -i '/nameserver 1.1.1.1/d' /etc/resolv.conf
 sed -i '/options edns0 trust-ad/d' /etc/resolv.conf
 sed -i '/search \./d' /etc/resolv.conf
-sed -i '/deb http:\/\/archive.ubuntu.com\/ubuntu jammy/d' /etc/apt/sources.list
-sed -i '/deb-src http:\/\/archive.ubuntu.com\/ubuntu jammy/d' /etc/apt/sources.list
-apt-get update -y
 modprobe tcp_cubic
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
@@ -17,3 +14,5 @@ sed -i '/nameserver/d' /etc/resolv.conf
 sh -c 'echo "nameserver 127.0.0.53" >> /etc/resolv.conf'
 systemctl restart systemd-resolved.service
 echo "All changes have been reverted."
+echo "Your system will reboot"
+reboot
