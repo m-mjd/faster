2ace85b14f7c2f69c2e9e63afbb17dc6f56ba29c
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-17 16:51:39 -0700

    faster.sh

--- faster.sh
+++ faster.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
-echo 'deb http://archive.ubuntu.com/ubuntu jammy main restricted' > /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy main restricted' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-updates main restricted' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-updates main restricted' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy universe' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy universe' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-updates universe' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-updates universe' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-updates multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-updates multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-security main restricted' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security main restricted' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy main restricted' > /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy main restricted' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-updates main restricted' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy-updates main restricted' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy universe' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy universe' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-updates universe' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy-updates universe' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy multiverse' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-updates multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy-updates multiverse' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-security main restricted' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy-security main restricted' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://us.archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
+sed -i '/nameserver/d' /etc/resolv.conf
+sh -c 'echo "nameserver 208.67.222.222" >> /etc/resolv.conf'
+sh -c 'echo "nameserver 208.67.220.220" >> /etc/resolv.conf'
+sh -c 'echo "nameserver 95.85.95.85" >> /etc/resolv.conf'
+sh -c 'echo "nameserver 2.56.220.2" >> /etc/resolv.conf'
+sh -c 'echo "nameserver 2001:4860:4860::8888" >> /etc/resolv.conf'
+sh -c 'echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf'
+sh -c 'echo "options edns0 trust-ad" >> /etc/resolv.conf'
+sh -c 'echo "search ." >> /etc/resolv.conf'
 apt-get update -y
+apt instal net-tools -y
+apt install resolvconf -y
 modprobe tcp_cubic
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 sh -c 'echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf'
 sh -c 'echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf'
 sysctl -p
-sed -i '/nameserver/d' /etc/resolv.conf
-sh -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
-sh -c 'echo "options edns0 trust-ad" >> /etc/resolv.conf'
-sh -c 'echo "search ." >> /etc/resolv.conf'
-sh -c 'echo "#!/bin/bash" > /usr/local/bin/update_resolv_conf.sh'
-sh -c 'echo "echo "nameserver 1.1.1.1" > /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
-sh -c 'echo "echo "options edns0 trust-ad" >> /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
-sh -c 'echo "echo "search ." >> /etc/resolv.conf" >> /usr/local/bin/update_resolv_conf.sh'
-chmod +x /usr/local/bin/update_resolv_conf.sh
-(crontab -l ; echo "*/15 * * * * /usr/local/bin/update_resolv_conf.sh") | crontab -
+
 sed -i '$a net.ipv4.icmp_echo_ignore_all=1' /etc/sysctl.conf
-chmod +x /usr/local/bin/update_resolv_conf.sh
 sysctl -p
 systemctl restart systemd-resolved.service
+echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'
+(crontab -l ; echo "0 */3 * * * curl https://speed.hetzner.de/1GB.bin -s -o /root/1GB.bin") | crontab -
 echo "Finish"
