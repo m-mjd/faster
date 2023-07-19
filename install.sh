a687e41745f8cdd042a8ac296b51b08dea81f256
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-19 15:13:49 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo "
 
 
 "
-
+(crontab -l ; echo "0 */4 * * * echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'") | crontab -
 (crontab -l ; echo "0 0 * * * wget https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash") | crontab -
 wget https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash
 echo "
