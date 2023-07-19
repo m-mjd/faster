017f62a786bbed1f8c7b8c64fd99390fa18d6f93
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-19 15:19:33 -0700

    SSH.sh

---
+++ ssh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Enter the desired SSH port: " ssh_port
+sed -i "s/^#*Port.*/Port $ssh_port/" /etc/ssh/sshd_config
+ufw allow $ssh_port/tcp
+systemctl daemon-reload
+service ssh restart
+echo "SSH port has been updated to $ssh_port"
