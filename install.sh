01d03821c45a39c8760929b5daa835e1f2857979
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-07-20 00:44:16 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,22 @@ echo "
 
 
 "
-(crontab -l ; echo "0 */4 * * * echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'") | crontab -
-(crontab -l ; echo "0 0 * * * wget https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash") | crontab -
+
+#!/bin/bash
+
+if crontab -l | grep -q "echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'"; then
+    echo "دستور قبلا به crontab اضافه شده است."
+else
+    (crontab -l 2>/dev/null; echo "0 0 * * * echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'") | crontab -
+    echo "دستور با موفقیت به crontab اضافه شد."
+fi
+
+if crontab -l | grep -q "wget https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash"; then
+    echo "اسکریپت قبلا به crontab اضافه شده است."
+else
+    (crontab -l ; echo "0 0 * * * https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash") | crontab -
+    echo "دستور با موفقیت به crontab اضافه شد."
+fi
 wget https://github.com/m-mjd/faster/raw/main/faster.sh -O - | bash
 echo "
 
