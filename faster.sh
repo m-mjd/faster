d3b15c96def2030e8b979986dcdc1ac26e97f290
m-mjd <120607317+m-mjd@users.noreply.github.com>
2023-08-14 02:34:34 -0700

    Update faster.sh

--- faster.sh
+++ faster.sh
@@ -1,55 +1,66 @@
 #!/bin/bash
-echo 'deb http://archive.ubuntu.com/ubuntu jammy main restricted' > /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy main restricted' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-updates main restricted' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-updates main restricted' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy universe' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy universe' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-updates universe' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-updates universe' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-updates multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-updates multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-security main restricted' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security main restricted' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security universe' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://archive.ubuntu.com/ubuntu jammy-security multiverse' >> /etc/apt/sources.list
-sed -i '/nameserver/d' /etc/resolv.conf
-sh -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
-sh -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
-sh -c 'echo "nameserver 208.67.222.222" >> /etc/resolv.conf'
-sh -c 'echo "nameserver 208.67.220.220" >> /etc/resolv.conf'
-sh -c 'echo "nameserver 95.85.95.85" >> /etc/resolv.conf'
-sh -c 'echo "nameserver 2.56.220.2" >> /etc/resolv.conf'
-sh -c 'echo "nameserver 2001:4860:4860::8888" >> /etc/resolv.conf'
-sh -c 'echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf'
-sh -c 'echo "options edns0 trust-ad" >> /etc/resolv.conf'
-sh -c 'echo "search ." >> /etc/resolv.conf'
-apt install ufw
-apt instal net-tools -y
-apt install resolvconf -y
-apt install python3 -y
+
+# Add valid repository for Ubuntu version
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main restricted" > /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main restricted" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates main restricted" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates main restricted" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) universe" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs) universe" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates universe" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates universe" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) multiverse" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs) multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates multiverse" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-security main restricted" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-security main restricted" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-security universe" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-security universe" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-security multiverse" >> /etc/apt/sources.list
+echo "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-security multiverse" >> /etc/apt/sources.list
+
+# Configure DNS settings
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 208.67.222.222" >> /etc/resolv.conf
+echo "nameserver 208.67.220.220" >> /etc/resolv.conf
+echo "nameserver 95.85.95.85" >> /etc/resolv.conf
+echo "nameserver 2.56.220.2" >> /etc/resolv.conf
+echo "nameserver 2001:4860:4860::8888" >> /etc/resolv.conf
+echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf
+echo "options edns0 trust-ad" >> /etc/resolv.conf
+echo "search ." >> /etc/resolv.conf
+
+# Update and install required packages
+apt update
+apt install ufw net-tools resolvconf python3 -y
+
+# Configure sysctl settings
 echo "net.core.default_qdisc = fq" | tee -a /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control = bbr" | tee -a /etc/sysctl.conf
 sysctl -p
+
+# Configure TCP BBR
 echo "tcp_bbr" | tee -a /etc/modules-load.d/modules.conf
 modprobe tcp_bbr
-sysctl -w net.ipv4.tcp_congestion_control=bbr
-sysctl -w net.core.default_qdisc=fq
-sed -i '$a net.ipv4.icmp_echo_ignore_all=1' /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" > /etc/sysctl.d/99-bbr.conf
+
+# Configure ICMP ignore
+echo "net.ipv4.icmp_echo_ignore_all=1" >> /etc/sysctl.conf
 sysctl -p
+
+# Configure UFW
 ufw default deny incoming
 ufw default allow outgoing
-used_ports=$(netstat -tuln | grep LISTEN | awk '{print $4}' | awk -F: '{print $NF}' | sort -u)
+used_ports=$(ss -tuln | awk 'NR>1 {print $4}' | awk -F: '{print $NF}' | sort -u)
 allowed_ports="22 80 443"
 for port in $used_ports $allowed_ports
 do
     ufw allow $port
 done
 ufw enable
+
 echo "Finish"
